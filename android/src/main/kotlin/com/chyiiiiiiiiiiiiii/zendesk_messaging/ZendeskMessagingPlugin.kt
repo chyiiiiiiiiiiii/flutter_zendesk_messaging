acfd724a99fceaab267fb20aa011a14f19e24148
Thibaud <tspoke@hotmail.fr>
2022-02-09 13:57:28 +0100

    Setup channel communication for android

--- android/src/main/kotlin/com/chyiiiiiiiiiiiiii/zendesk_messaging/ZendeskMessagingPlugin.kt
+++ android/src/main/kotlin/com/chyiiiiiiiiiiiiii/zendesk_messaging/ZendeskMessagingPlugin.kt
@@ -2,11 +2,7 @@ package com.chyiiiiiiiiiiiiii.zendesk_messaging
 
 import ZendeskMessaging
 import android.app.Activity
-import android.content.Intent
-import android.os.Build
 import androidx.annotation.NonNull
-import androidx.annotation.RequiresApi
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -16,27 +12,24 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** ZendeskMessagingPlugin */
-class ZendeskMessagingPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+class ZendeskMessagingPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
     private val tag = "[ZendeskMessagingPlugin]"
-
-    private lateinit var channel : MethodChannel
-
+    private lateinit var channel: MethodChannel
     var activity: Activity? = null
-
     var isInitialize: Boolean = false
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         val sendData: Any? = call.arguments
-        ///
         val zendeskMessaging = ZendeskMessaging(this, channel)
+
         when (call.method) {
             "initialize" -> {
                 if (isInitialize) {
-                    println("$tag - Messaging is already initialize!")
+                    println("$tag - Messaging is already initialized!")
                     return
                 }
                 val channelKey = call.argument<String>("channelKey")!!
@@ -44,15 +37,41 @@ class ZendeskMessagingPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
             }
             "show" -> {
                 if (!isInitialize) {
-                    println("$tag - Messaging needs to initialize first")
+                    println("$tag - Messaging needs to be initialized first")
                     return
                 }
                 zendeskMessaging.show()
             }
+            "loginUser" -> {
+                if (!isInitialize) {
+                    println("$tag - Messaging needs to be initialized first")
+                    return
+                }
+
+                try {
+                    val jwt = call.argument<String>("jwt")
+                    if (jwt == null || jwt.isEmpty()) {
+                        throw Exception("JWT is empty or null")
+                    }
+                    zendeskMessaging.loginUser(jwt)
+                } catch (err: Throwable) {
+                    println("$tag - Messaging::login invalid arguments. {'jwt': '<your_jwt>'} expected !")
+                    println(err.message)
+                    return
+                }
+            }
+            "logoutUser" -> {
+                if (!isInitialize) {
+                    println("$tag - Messaging needs to be initialized first")
+                    return
+                }
+                zendeskMessaging.logoutUser()
+            }
             else -> {
-              result.notImplemented()
+                result.notImplemented()
             }
         }
+
         if (sendData != null) {
             result.success(sendData)
         } else {
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,55 +13,58 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String androidChannelKey =
-      'eyJzZXR0aW5nc191cmwiOiJodHRwczovL2hhbmFtaWhlbHAuemVuZGVzay5jb20vbW9iaWxlX3Nka19hcGkvc2V0dGluZ3MvMDFGR0tDRTlSNEFLWDBGOUc2Sk04Mk5RQU0uanNvbiJ9';
-  final String iosChannelKey =
-      'eyJzZXR0aW5nc191cmwiOiJodHRwczovL2hhbmFtaWhlbHAuemVuZGVzay5jb20vbW9iaWxlX3Nka19hcGkvc2V0dGluZ3MvMDFGR1BGVFQ1Q1hFRjdRWVkwUkg2R0JYS0MuanNvbiJ9';
+  static const String androidChannelKey = "eyJzZXR0aW5nc191cmwiOiJodHRwczovL2hhbmFtaWhlbHAuemVuZGVzay5jb20vbW9iaWxlX3Nka19hcGkvc2V0dGluZ3MvMDFGR0tDRTlSNEFLWDBGOUc2Sk04Mk5RQU0uanNvbiJ9";
+  static const String iosChannelKey = "eyJzZXR0aW5nc191cmwiOiJodHRwczovL2hhbmFtaWhlbHAuemVuZGVzay5jb20vbW9iaWxlX3Nka19hcGkvc2V0dGluZ3MvMDFGR1BGVFQ1Q1hFRjdRWVkwUkg2R0JYS0MuanNvbiJ9";
+
+  final List<String> channelMessages = [];
 
   @override
   void initState() {
     super.initState();
-    ZendeskMessaging.initialize(
-      androidChannelKey: androidChannelKey,
-      iosChannelKey: iosChannelKey,
-    );
+    // Optional, observe all incoming messages
+    ZendeskMessaging.setMessageHandler((type, arguments) {
+      setState(() {
+        channelMessages.add("$type - args=$arguments");
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    var message = channelMessages.join("\n");
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Zendesk Messaging Example'),
         ),
-        body: Center(
+        body: SafeArea(
           child: Container(
-            padding: const EdgeInsets.all(12.0),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(20.0),
-              boxShadow: [
-                const BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(2.0, 2.0),
-                )
-              ],
-            ),
-            child: GestureDetector(
-              onTap: () {
-                ZendeskMessaging.show();
-              },
-              child: const Text(
-                "Messaging",
-                style: TextStyle(
-                  color: Colors.amberAccent,
-                  fontSize: 24.0,
+            padding: const EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Text(message),
+                ElevatedButton(
+                  onPressed: () => ZendeskMessaging.initialize(androidChannelKey: androidChannelKey, iosChannelKey: iosChannelKey),
+                  child: const Text("Initialize"),
                 ),
-              ),
+                ElevatedButton(onPressed: () => ZendeskMessaging.show(), child: const Text("Show messaging")),
+                ElevatedButton(onPressed: () => _login(), child: const Text("Login")),
+                ElevatedButton(onPressed: () => ZendeskMessaging.logoutUser(), child: const Text("Logout")),
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  void _login() {
+    // You can attach local observer when calling some methods to be notified when ready
+    ZendeskMessaging.loginUser(
+      jwt: "my_jwt",
+      onSuccess: (id, externalId) => setState(() => channelMessages.add("Login observer SUCCESS: $id, $externalId")),
+      onFailure: () => setState(() => channelMessages.add("Login observer FAILURE !")),
     );
   }
 }
